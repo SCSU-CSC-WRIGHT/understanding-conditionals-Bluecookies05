2e925a288ebedf316f049b63dbbf89d158663c23
Bluecookies05 <98850306+Bluecookies05@users.noreply.github.com>
2024-10-02 20:30:53 +0000

    checking if this actually pushes

---
+++ Lab_3.py
@@ -0,0 +1,45 @@
+## ============ temperature check ================
+temp_check = int(input("What is the weather today?: "))
+
+if temp_check>30: 
+    print(" Its a hot day! ")
+else: 
+    print(" the weather is cool  ")
+
+## ============ Number comparision ===============
+
+number1 = int(input(" what is the first number? "))
+number2 = int(input(" what is the secpnd number? "))
+
+if number1<number2:
+    print( number2 )
+elif number1>number2:
+    print( number1 )
+else:  
+    print(" both numbers are the same! ")
+
+## ============ password checker =================
+user_password = int(input(" type your password in: "))
+admin_password = admin 
+
+if admin_password==user_password:
+    print("access granted!")
+else:
+    print("STRANGER ALERT GET OUT!!!!")
+## ============ simple calculatior ===============
+
+number_1 = int(input(" what is the first number? "))
+number_2 = int(input(" what is the secpnd number? "))
+
+## ============ positive, negative or 0 ==========
+
+mystery = int(input(" what is your number?:  "))
+
+if mystery>1:
+    print(" your number is a positive! ")
+elif mystery<1:
+    print(" your number is less than one! ")
+else:
+    print(" your number is equal to 0!! ")
+
+## ============ day of the week ==================
