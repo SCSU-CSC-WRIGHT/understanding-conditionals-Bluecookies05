d81e18fc39bd60e9d4950d746955176caefd5afd
Bluecookies05 <98850306+Bluecookies05@users.noreply.github.com>
2024-10-09 13:33:08 +0000

    finished version

--- Lab_3.py
+++ Lab_3.py
@@ -1,5 +1,5 @@
 ## ============ temperature check ================
-temp_check = int(input("What is the weather today?: "))
+temp_check = int(input("What is the weather temp today in celcuis?: "))
 
 if temp_check>30: 
     print(" Its a hot day! ")
@@ -19,29 +19,30 @@ else:
     print(" both numbers are the same! ")
 
 ## ============ password checker =================
+user_password = input("Type your password in: ")
+admin_password = "admin"  
 
-user_password = int(input(" type your password in: "))
-admin_password = ("admin")
-
-if admin_password==user_password:
-    print("access granted!")
+if admin_password == user_password:
+    print("Access granted!")
 else:
     print("STRANGER ALERT GET OUT!!!!")
 
 ## ============ simple calculatior ===============
 
-number_1 = int(input(" what is the first number? "))
-number_2 = int(input(" what is the secpnd number? "))
-op = int(input("enter op"))
+number_1 = int(input("What is the first number? "))
+number_2 = int(input("What is the second number? "))
+op = input("Enter operator (+, -, *, /): ") 
 
-if ( op== '+' ):
+if op == '+':
     print(number_1 + number_2)
-elif ( op== '-' ):
+elif op == '-':
     print(number_1 - number_2)
-elif ( op== '*' ):
+elif op == '*':
     print(number_1 * number_2)
-elif ( op== '/' ):
+elif op == '/':
     print(number_1 / number_2)
+else:
+    print("Invalid operator!")
 
 ## ============ positive, negative or 0 ==========
 
@@ -56,3 +57,21 @@ else:
 
 ## ============ day of the week ==================
 
+day_of_week = int(input("Put a number between 1-7: "))
+
+if day_of_week == 1:
+    print("Monday")
+elif day_of_week == 2:
+    print("Tuesday")    
+elif day_of_week == 3:
+    print("Wednesday")
+elif day_of_week == 4:
+    print("Thursday")
+elif day_of_week == 5:
+    print("Friday")
+elif day_of_week == 6:
+    print("Saturday")
+elif day_of_week == 7:
+    print("Sunday")
+else:
+    print("Invalid number! Please enter a number between 1 and 7.")
